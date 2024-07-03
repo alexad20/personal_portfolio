4d805828c621f823088db706f5398a76cd97e015
alexad20 <alexatolentino20@gmail.com>
2024-07-03 18:59:28 -0400

    my changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,14 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll('.nav a');
     const contentWrapper = document.querySelector('.content_wrapper');
     const skillsLogos = document.querySelectorAll('.logos img');
-
+    const avatar = document.querySelectorAll('.image img');
 
     function checkSidebarWidth() {
         console.log('Sidebar width:', sidebar.offsetWidth);
         if (sidebar.offsetWidth < 122) {
             navLinks.forEach(link => link.classList.add('hide-text'));
+            avatar.forEach(img => img.classList.add('size2'));
         } else {
             navLinks.forEach(link => link.classList.remove('hide-text'));
+            avatar.forEach(img => img.classList.remove('size2'));
         }
     }
 
@@ -18,8 +20,10 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log('Screen width:', contentWrapper.offsetWidth);
         if (contentWrapper.offsetWidth < 800) {
             skillsLogos.forEach(img => img.classList.add('size'));
+           
         } else {
             skillsLogos.forEach(img => img.classList.remove('size'));
+           
         }
     }
 
